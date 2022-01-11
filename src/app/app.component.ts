eaeb0923ff0b5d9750f8d694cbdd26c1d6613b85
Chris Sanchez <Chris.Sanchez@finastra.com>
2022-01-11 12:15:10 -0800

    setup webviewer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Output, ViewChild } from '@angular/core';
+import WebViewer, { WebViewerInstance } from '@pdftron/webviewer';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,57 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  @ViewChild('webviewer') viewer: ElementRef;
+  wvInstance: WebViewerInstance;
   title = 'pdftron-wasm-repro';
+  @Output() coreControlsEvent: EventEmitter<string> = new EventEmitter();
+
+  private documentLoaded$: Subject<void>;
+
+  configOptions = {
+    fullAPI: true,
+    path: '../../assets',
+    extension: 'pdf',
+    annotationUser: '',
+    initialDoc: '../files/webviewer-demo-annotated.pdf'
+  };
+
+  constructor() {
+    this.documentLoaded$ = new Subject<void>();
+  }
+
+  ngAfterViewInit(): void {
+    WebViewer(this.configOptions, this.viewer.nativeElement).then(instance => {
+      this.wvInstance = instance;
+
+      this.coreControlsEvent.emit(instance.UI.LayoutMode.Single);
+
+      const { documentViewer, Annotations, annotationManager } = instance.Core;
+
+      instance.UI.openElements(['notesPanel']);
+
+      documentViewer.addEventListener('annotationsLoaded', () => {
+        console.log('annotations loaded');
+      });
+
+      documentViewer.addEventListener('documentLoaded', () => {
+        this.documentLoaded$.next();
+        const rectangleAnnot = new Annotations.RectangleAnnotation({
+          PageNumber: 1,
+          // values are in page coordinates with (0, 0) in the top left
+          X: 100,
+          Y: 150,
+          Width: 200,
+          Height: 50,
+          Author: annotationManager.getCurrentUser()
+        });
+        annotationManager.addAnnotation(rectangleAnnot);
+        annotationManager.redrawAnnotation(rectangleAnnot);
+      });
+    })
+  }
+
+  getDocumentLoadedObservable() {
+    return this.documentLoaded$.asObservable();
+  }
 }
